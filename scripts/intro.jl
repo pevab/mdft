eb1e1c5502b876b759f139b8fd3cd5bde7061f8f
Peva Blanchard <pblanchard@kleis.ch>
2025-07-03 22:20:48 +0100

    similarity effect

--- scripts/intro.jl
+++ scripts/intro.jl
@@ -13,23 +13,15 @@ abstract type  AttentionProcess
 end
 
 struct BernoulliAttentionProcess <: AttentionProcess
-    probabilities :: Vector{Float64}
+    probs :: Vector{Float64}
 end
 
 function next(process :: BernoulliAttentionProcess) :: Vector{Float64}
-    probabilities = process.probabilities
-    n_attributes = length(probabilities)
+    probs = process.probs
+    n_attributes = length(probs)
+    focus = rand(Categorical(probs))
     result = zeros(n_attributes)
-    for a in 1:n_attributes
-        p = probabilities[a]
-        result[a] = rand(Bernoulli(p))
-    end
-    # n = count(==(1), result)
-    # if n > 0
-    #     return result / count(==(1), result)
-    # else
-    #     return result
-    # end
+    result[focus] = 1
     result
 end
 
@@ -102,50 +94,94 @@ end
 Examples
 =#
 
+function contrast_matrix(n_alternatives) :: Matrix{Float64}
+    f = 1/(n_alternatives - 1)
+    (1 + f) * Matrix(I, n_alternatives, n_alternatives) - f * ones((n_alternatives, n_alternatives))
+end
+
+function sym(m :: Matrix{Float64}) :: Matrix{Float64}
+    (m + m')/2
+end
+
 function similarity_effect()
-    model = MDFT(
+    attention_process = BernoulliAttentionProcess([.49, .51])
+    model2 = MDFT(
+        2, # alternatives (cars) A B
+        2, # attributes E (economy) and Q (quality)
+        [ # personal evaluation matrix M
+            # E     Q
+            1.00  3.00; # A
+            3.00  1.00; # B
+        ],
+        # attention weight process
+        attention_process,
+        # contrast matrix
+        contrast_matrix(2),
+        # residual error law
+        Dirac(0),
+        sym([ # feedback matrix S
+            .940 .000;
+            .000 .940;
+        ])
+    )
+    model3 = MDFT(
         3, # alternatives (cars) A S B
         2, # attributes E (economy) and Q (quality)
         [ # personal evaluation matrix M
             # E     Q
             1.00  3.00; # A
-            1.25  2.8; # S
+            0.99  3.01; # S
             3.00  1.00; # B
         ],
         # attention weight process
-        BernoulliAttentionProcess([.45, .45]),
-        [ # contrast matrix C
-            1 -0.5 -0.5;
-            -0.5 1 -0.5;
-            -0.5 -0.5 1;
-        ],
+        attention_process,
+        # contrast matrix
+        contrast_matrix(3),
         # residual error law
         Dirac(0),
-        [ # feedback matrix S
+        sym([ # feedback matrix S
             .940 .000 .000;
             .000 .940 .000;
             .000 .000 .940;
-        ]
+        ])
     )
     function winner(p :: Vector{Float64}) :: Vector{Float64}
         probs = softmax(p)
         w = rand(Categorical(probs))
-        r = zeros(3)
+        r = zeros(size(p))
         r[w] = 1
         r
     end
 
     max_time = 100
-    n_seeds = 500
-    results = zeros((3, max_time))
+    n_seeds = 1000
+
+    # model2
+    results = zeros((2, max_time))
     for deadline in 1:max_time
-        s = zeros((3,0))
+        s = zeros((2,0))
         for _ in 1:n_seeds
-            history = run(model, DeadlineRule(deadline))
+            history = run(model2, DeadlineRule(deadline))
             p = history[:, end]
             s = hcat(s, winner(p))
         end
         results[:, deadline] = mean(s, dims=2)
     end
-    plot(results', labels=['A' 'S' 'B'])
+    plt2 = plot(results', labels=['A' 'B'], linecolor=[:blue :green])
+
+    # model3
+    results = zeros((3, max_time))
+    for deadline in 1:max_time
+        s = zeros((3,0))
+        for _ in 1:n_seeds
+            history = run(model3, DeadlineRule(deadline))
+            p = history[:, end]
+            s = hcat(s, winner(p))
+        end
+        results[:, deadline] = mean(s, dims=2)
+    end
+    plt3 = plot(results', labels=['A' 'S' 'B'], linecolor=[:blue :red :green])
+
+    # plot
+    plot(plt2, plt3, layout=(1,2))
 end
